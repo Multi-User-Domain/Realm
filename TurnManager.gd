4049f2ab4a1d455b1e6ece99fd5ba68dc5a8ed68
Calum Mackervoy <c.mackervoy@gmail.com>
2023-05-03 19:26:58 +0200

    added TurnManager component

---
+++ TurnManager.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+onready var game = get_tree().current_scene
+
+# TODO: the purpose of this Node is to manage turns, i.e.
+# the actions of each active card on each cycle,
+# the actions of players on each turn (when they can play cards)
+# https://github.com/Multi-User-Domain/games-transformed-jam-2023/issues/4
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
