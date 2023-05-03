2474d11e7492103dfd828eee02b03fb0be5327c9
Calum Mackervoy <c.mackervoy@gmail.com>
2023-05-03 13:50:40 +0200

    redesign layout

--- Main.gd
+++ Main.gd
@@ -1,15 +1,13 @@
 extends Node2D
 
 
-# Declare member variables here, e.g.
-# var a = 2
-
 # when the node is loaded into the scene we fetch references to its children
 onready var HUD = get_node("HUD")
-onready var player1_hand = get_node("Player1Hand")
-onready var player1_active = get_node("Player1Active")
-onready var player2_active = get_node("Player2Active")
-onready var player2_hand = get_node("Player2Hand")
+onready var player1_avatar = get_node("Player1Avatar")
+onready var player1_cards = get_node("Player1Cards")
+onready var player2_cards = get_node("Player2Cards")
+onready var player2_avatar = get_node("Player2Avatar")
+onready var card_tray = get_node("CardTray")
 
 # preload scenes that we want to instantiate programatically
 var card_scene = preload("res://obj/card/Card.tscn")
@@ -24,21 +22,31 @@ func _add_card_to_node(node: Node2D):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# init game
-	# position parts of the UI
+	# position the UI in segments
 	var viewport_size = get_viewport().size
 	var y_margin = 5
 	var quarter_height = viewport_size.y * 0.25
 	var x_margin = 10
-	player1_hand.set_position(Vector2(x_margin, y_margin))
-	player1_active.set_position(Vector2(x_margin, y_margin + quarter_height))
-	player2_active.set_position(Vector2(x_margin, y_margin + (quarter_height * 2)))
-	player2_hand.set_position(Vector2(x_margin, y_margin + (quarter_height * 3)))
+	
+	# top 3 quarters for the arena
+	var three_quarters = quarter_height * 3
+	var arena_quarter = three_quarters * 0.25
+	
+	# of that split into 4 (each player has an avatar and a card field)
+	player1_avatar.set_position(Vector2(x_margin, y_margin))
+	player1_cards.set_position(Vector2(x_margin, y_margin + arena_quarter))
+	player2_cards.set_position(Vector2(x_margin, y_margin + (arena_quarter * 2)))
+	player2_avatar.set_position(Vector2(x_margin, y_margin + (arena_quarter * 3)))
+	
+	# bottom quarter for player card actions
+	card_tray.set_position(Vector2(x_margin, y_margin + (quarter_height * 3)))
 	
 	# spawn placeholder cards to test placement
-	_add_card_to_node(player1_hand)
-	_add_card_to_node(player1_active)
-	_add_card_to_node(player2_active)
-	_add_card_to_node(player2_hand)
+	_add_card_to_node(player1_avatar)
+	_add_card_to_node(player1_cards)
+	_add_card_to_node(player2_cards)
+	_add_card_to_node(player2_avatar)
+	_add_card_to_node(card_tray)
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
--- obj/card/Card.gd
+++ obj/card/Card.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+# Declare member variables here, e.g.
+# var a = 2
 
 onready var game = get_tree().current_scene
 
