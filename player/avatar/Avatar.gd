cf5cf2eb871477bde814dd7446ee4bf651e539d2
Calum Mackervoy <c.mackervoy@gmail.com>
2023-05-03 17:11:04 +0200

    added name label to player avatars

--- player/avatar/Avatar.gd
+++ player/avatar/Avatar.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var portrait_sprite = get_node("PortraitSprite")
+onready var name_label = get_node("NameLabel")
 export var default_hp = 100
 var jsonld_store = {}
 
@@ -31,9 +32,13 @@ func init_new_player(character_jsonld):
 	# TODO: https://github.com/Multi-User-Domain/games-transformed-jam-2023/issues/1
 	# 128, 128 with the in-built textures
 	portrait_sprite.set_scale(Vector2(0.25, 0.25))
-	var half_portrait = Vector2(64, 64) # also needs to become relative to size
+	var portrait_size = Vector2(128, 128) # also needs to become relative to size
+	var half_portrait = portrait_size * 0.5
 	# centre along the x axis
 	portrait_sprite.set_position(Vector2(get_viewport_rect().size.x * 0.5, self.position.y) + half_portrait)
+	
+	name_label.set_text(get_rdf_property("n:fn"))
+	name_label.set_position(portrait_sprite.position + Vector2(-half_portrait.x + 1, half_portrait.y + 1))
 
 # TODO: find a more DRY way to do this across nodes
 func get_rdf_property(property):
