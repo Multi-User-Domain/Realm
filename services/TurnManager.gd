fa561ed6a43b08a63a510b74856ec1a45c90a444
Calum Mackervoy <c.mackervoy@gmail.com>
2023-05-07 15:07:03 +0200

    TurnManager tracks elapsed turns

--- services/TurnManager.gd
+++ services/TurnManager.gd
@@ -8,6 +8,10 @@ onready var card_action_timer = get_node("CardActionTimer")
 # the actions of each active card on each cycle,
 # the actions of players on each turn (when they can play cards)
 
+# keep track of how many player and card turns have been completed
+var elapsed_player_turns = 0
+var elapsed_card_turns = 0
+
 func start():
 	# start game timer
 	turn_timer.start()
@@ -20,12 +24,13 @@ func _on_TurnTimer_timeout():
 	game.player2_avatar.play_cards()
 
 func _play_card_actions(player_avatar_scene, opponent_avatar_scene):
-	for card in player_avatar_scene.card_manager.play_card_actions():
-		if card["@id"] == Globals.BUILT_IN_ACTIONS.BASIC_ATTACK:
-			opponent_avatar_scene.health_bar.remove_health(1)
+	elapsed_card_turns += 1
+	for action in player_avatar_scene.card_manager.play_card_actions():
+		# card.take_turn(elapsed_card_turns, opponent_avatar_scene, opponent_avatar_scene.card_manager.active_cards)
+		opponent_avatar_scene.health_bar.remove_health(1)
 
 # allow active cards to make attacks
 func _on_CardActionTimer_timeout():
-	# TODO: using a timer for each card (e.g. each action is completed every x seconds)
+	elapsed_player_turns += 1
 	self._play_card_actions(game.player1_avatar, game.player2_avatar)
 	self._play_card_actions(game.player2_avatar, game.player1_avatar)
